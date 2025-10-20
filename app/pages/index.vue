4e9429b6f7671093304ae880fd25b7d288405f21
hobobemo <92595040+hobobemo@users.noreply.github.com>
2025-10-20 15:40:39 -0600

    add home page info

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,6 +16,22 @@ useSeoMeta({
 
 <template>
   <div v-if="page">
-    {{ page }}
+    <SharedHeader :title="page.hero.title" :description="page.hero.description" :links="page.hero.links" :image="page.hero.image"/>
+
+    <UPageSection :description="page.features.description">
+      <template #title>
+        <MDC :value="page.features.title" />
+      </template>
+
+      <UPageGrid>
+        <UPageCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" spotlight />
+      </UPageGrid>
+    </UPageSection>
+
+    <USeparator />
+
+    <UPageCTA v-bind="page.cta" variant="naked" class="overflow-hidden">
+      <div class="absolute rounded-full dark:bg-primary blur-[250px] size-40 sm:size-50 transform -translate-x-1/2 left-1/2 -translate-y-80" />
+    </UPageCTA>
   </div>
 </template>
